6f7c221aabc296fae29a2ee450eb20bbdf47ca44
patriotkusuma <patriot.kusumasejati@gmail.co>
2020-12-02 01:03:34 +0700

    Fixing Something

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php 
 // memanggil koneksi database
-$server = "localhost";
-$user = "root";
-$pass = "";
-$database = "dblatihan";
+$server = "us-cdbr-east-02.cleardb.com";
+$user = "b848dcd379f31a";
+$pass = "efed4f22";
+$database = "heroku_73d1e5bc68e7e25";
 
 $koneksi = mysqli_connect($server, $user, $pass, $database)or die(mysqli_error($koneksi));
